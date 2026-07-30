36b901f101a72b8a43222be24ca13289704c8b14
vaibhavj2006 <168920428+vaibhavj2006@users.noreply.github.com>
2026-07-30 20:13:01 +0530

    Fix address routes parameter and HTTP methods

--- server/routes/addressRoutes.js
+++ server/routes/addressRoutes.js
@@ -10,10 +10,19 @@ const { protect } = require('../middleware/auth');
 
 const router = express.Router();
 
+// Get all user addresses & Add a new address
 router.get('/', protect, getAddresses);
 router.post('/', protect, addAddress);
+
+// Update address details (supports PUT and PATCH)
 router.put('/:addressId', protect, updateAddress);
+router.patch('/:addressId', protect, updateAddress);
+
+// Delete an address
 router.delete('/:addressId', protect, deleteAddress);
+
+// Set default address (supports PUT and PATCH)
 router.put('/:addressId/default', protect, setDefaultAddress);
+router.patch('/:addressId/default', protect, setDefaultAddress);
 
 module.exports = router;
